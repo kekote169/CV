bc1ac6bee34b9f35e15e55976115c8be959f59ef
kekote169 <36438413+kekote169@users.noreply.github.com>
2020-12-28 21:52:53 +0100

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,29 @@
+var w = document.getElementById('aboutMe');
+var x = document.getElementById('resume');
+var y = document.getElementById('portfolio');
+var z = document.getElementById('contact');
+
+function btn0() {
+w.style.display = 'flex';
+x.style.display = 'none';
+y.style.display = 'none';
+z.style.display = 'none';
+}
+function btn1() {
+w.style.display = 'none';
+x.style.display = 'flex';
+y.style.display = 'none';
+z.style.display = 'none';
+}
+function btn2() {
+w.style.display = 'none';
+x.style.display = 'none';
+y.style.display = 'flex';
+z.style.display = 'none';
+}
+function btn3() {
+w.style.display = 'none';
+x.style.display = 'none';
+y.style.display = 'none';
+z.style.display = 'flex';
+}
